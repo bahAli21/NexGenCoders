0f8d654514ad51b95ff9c2210a9910e22d227d5d
Mamadou BAH <bah311802@gmail.com>
2024-05-13 04:12:19 +0200

    feat: Implement main script to use imported modules
    Created a new JavaScript file named 'script.js' to serve as the main script for the website. This file imports and utilizes modules such as 'events.js', 'variables.js', and other necessary modules to manage the functionality of the header and login features.
    
    #javascript #script #modules #header #login

--- NexGenCoders/assets/js/script.js
+++ NexGenCoders/assets/js/script.js
@@ -1,5 +1,10 @@
 
 //Très important , pour les evenements il faut juste importer le modules events qui gère les evenements
 
+/*J'importe le module functions*/
+import {showMenu} from './functions.js';
+
 /*J'importe le module events*/
 import {} from './events.js';
+
+showMenu('nav-toggle','nav-menu'); //Est coder dans le module Functions qui est ensuite exporter par events donc on a acces ici
