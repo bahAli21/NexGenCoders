02111acd36077d1d517c4fa49a714f09de1bad99
Mamadou BAH <bah311802@gmail.com>
2024-05-13 04:11:01 +0200

    feat: Create variables.js to store DOM elements
    Added a new JavaScript file named 'variables.js' to store all DOM elements used in the header and login functionalities. This file consolidates the references to HTML elements, making it easier to manage and maintain the code.
    
    #javascript #variables #dom #header #login

--- NexGenCoders/assets/js/variables.js
+++ NexGenCoders/assets/js/variables.js
@@ -7,5 +7,7 @@ const loginDiv = document.querySelector('.login');
 const registerDiv = document.querySelector('.register');
 const backToSignin = document.querySelector('#back-to-sign-in');
 
+const resetPasswordLink = document.querySelector('#forgot-Password');
+const resetPassword = document.querySelector('.reset-password');
 // Export des variables pour les rendre disponibles pour d'autres modules
-export { SignIn, SignUp, loginDiv, registerDiv, backToSignin };
+export { SignIn, SignUp, loginDiv, registerDiv, backToSignin, resetPasswordLink, resetPassword };
