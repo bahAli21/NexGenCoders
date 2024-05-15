563604fa2f4eed3f7d402f9227ee3ddbe69b64cf
diallosidy <diallosidymohamed99@gmail.com>
2024-05-15 03:52:52 +0200

    Added Routes file for managing page routing

---
+++ NexGenCoders/inc/Routes.php
@@ -0,0 +1,17 @@
+<?php
+
+$Routes = [
+    "signin" => [
+        "controller" => "SignInController",
+        "template" => "signin"
+    ],
+
+    'signup' => [
+        'controller' => "SignController",
+        'template' => "signup"
+    ],
+    'home' => [
+        'controller' => "HomeController",
+        'template' => "home"
+    ]
+];
